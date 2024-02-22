cb90947c508be9bd4221745c53d387010651f8fe
gw <69770151+jinjinjojo@users.noreply.github.com>
2024-02-22 09:10:01 -0800

    Update uv.config.js

--- prox/uv/uv.config.js
+++ prox/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/prox/vexxed-browser/',
-    bare: 'https://bare2.onrender.com/',
+    bare: 'https://flamepass.guard.lol/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/prox/uv/uv.handler.js',
