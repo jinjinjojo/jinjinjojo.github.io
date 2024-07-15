56966660b99b7923b34f355274457c86bc0a1a62
Test <69770151+jinjinjojo@users.noreply.github.com>
2024-07-15 09:26:32 -0700

    test

---
+++ STATIC_officialflamepass-main/prox/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/prox/0c2f1ddc-5748-436f-ae7c-89b5f708d90e/',
+    bare: 'https://sellix-key-checker-2sfjgz2adq-uc.a.run.app/0acc41f5-6f26-4480-ac31-3f72706a2fc8/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/prox/uv/uv.handler.js',
+    bundle: '/prox/uv/uv.bundle.js',
+    config: '/prox/uv/uv.config.js',
+    sw: '/prox/uv/uv.sw.js',
+};
